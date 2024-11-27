2e90860e020dc0d80f012204b356dc3f41f41efe
Timm Mutzel <timm.mutzel@gmx.de>
2024-11-27 20:52:51 +0100

    Added Account & Transaction functionality

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/suave/sdk"
 	"github.com/flashbots/suapp-examples/framework"
 )
 
@@ -57,14 +59,13 @@ func importKs() {
 	}
 }
 
-func deployContract() *framework.Contract {
-	fr := framework.New()
-	return fr.Suave.DeployContract("SealedAuction.sol/SealedAuction.json")
+func deployContract(_path string) *framework.Contract {
+	return fr.Suave.DeployContract(_path)
 }
 
 // TODO: does not work so far for constructor with args, need to uncomment constructor in contract
 func deployContractWithConstructor() *bind.BoundContract {
-	artifact, err := framework.ReadArtifact("SealedAuction.sol/SealedAuction.json")
+	artifact, err := framework.ReadArtifact(path)
 	if err != nil {
 		panic(err)
 	}
@@ -98,7 +99,7 @@ func deployContractWithConstructor() *bind.BoundContract {
 // only prints latest emitted event; flag to request new  event
 func printInfo(contract *framework.Contract) {
 	receipt := contract.SendConfidentialRequest("printInfo", nil, nil)
-	event, err := contract.Abi.Events["AuctionOpened"].ParseLog(receipt.Logs[len(receipt.Logs)-1])
+	event, err := contract.Abi.Events["AuctionOpened"].ParseLog(receipt.Logs[0])
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -111,7 +112,7 @@ func registerRPC(contract *framework.Contract) {
 	confidentialInput := "https://sepolia.infura.io/v3/93302e94e89f41afafa250f8dce33086"
 	chainID := big.NewInt(11155111)
 	receipt := contract.SendConfidentialRequest("registerRPCOffchain", []interface{}{chainID}, []byte(confidentialInput))
-	event, err := contract.Abi.Events["RPCEndpointUpdated"].ParseLog(receipt.Logs[len(receipt.Logs)-1])
+	event, err := contract.Abi.Events["RPCEndpointUpdated"].ParseLog(receipt.Logs[0])
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -120,35 +121,97 @@ func registerRPC(contract *framework.Contract) {
 
 func getBiddingAddress(contract *framework.Contract) {
 	receipt := contract.SendConfidentialRequest("getBiddingAddress", nil, nil)
-	event, err := contract.Abi.Events["BiddingAddress"].ParseLog(receipt.Logs[len(receipt.Logs)-1])
+	event, err := contract.Abi.Events["BiddingAddress"].ParseLog(receipt.Logs[0])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Owner of Bidding address:", event["owner"])
 	fmt.Println("Encoded L1 address:", event["encodedL1Address"])
-
 }
 
-var client *ethclient.Client
+func createAccount() *framework.PrivKey {
+	testAddr := framework.GeneratePrivKey()
+	log.Printf("Created Address at: %s", testAddr.Address().Hex())
+	fundAccount(testAddr)
+	return testAddr
+}
 
-func main() {
-	// suave-geth --suave.dev --suave.eth.external-whitelist='*'
-	// TODO: install go dependencies: create go.mod from example
-	// forge build
-	// copy SealedAuction.json to go/pkg (see example below)
-	// sudo cp ./out/SealedAuction.sol/SealedAuction.json  /home/timm/go/pkg/mod/github.com/flashbots/suapp-examples@v0.0.0-20241031122241-896ca6742979/out/SealedAuction.sol/SealedAuction.json
-	// go run main.go
+func fundAccount(privKey *framework.PrivKey) {
+	fundBalance := big.NewInt(100000000000000000)
+	if err := fr.Suave.FundAccount(privKey.Address(), fundBalance); err != nil {
+		log.Fatal(err)
+	} else {
+		if bal, err := client.BalanceAt(context.Background(), privKey.Address(), nil); err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Println("Balance of account: ", privKey.Address(), ":\t", bal)
+		}
+	}
+}
 
+// TODO: adapt to something useful
+func makeTransaction(privKey *framework.PrivKey) {
+	newClient := sdk.NewClient(client.Client(), privKey.Priv, fr.KettleAddress)
+	gas := big.NewInt(10000)
+	txn := &types.LegacyTx{
+		Nonce:    1,
+		To:       &fr.KettleAddress,
+		Value:    gas,
+		Gas:      10000000,
+		GasPrice: gas,
+		Data:     nil,
+	}
+	res, err := newClient.SendTransaction(txn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Hash of Transaction: ", res.Hash())
+}
+
+// helper functionality
+func printChainInfoComplete() {
+	blockNumber, err := client.BlockNumber(context.Background())
+	printChainInfo("Chain Block Count: ", blockNumber, err)
+	chainID, err := client.ChainID(context.Background())
+	printChainInfo("Chain ID: ", chainID, err)
+	peerCount, err := client.PeerCount(context.Background())
+	printChainInfo("Peer count: ", peerCount, err)
+}
+
+func printChainInfo(mes string, out interface{}, err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(mes, out)
+}
+
+// called before main()
+func init() {
 	var err error
 	client, err = ethclient.Dial("http://localhost:8545")
 	if err != nil {
 		panic(err)
 	}
+	fr = framework.New()
+}
+
+var client *ethclient.Client
+var path = "SealedAuction.sol/SealedAuction.json"
+var fr *framework.Framework
+
+func main() {
+	// suave-geth --suave.dev --suave.eth.external-whitelist='*'
+	// TODO: install go dependencies: create go.mod from example add replace line
+	// forge build
+	// copy SealedAuction.json to go/pkg (see example below)
+	// sudo cp ./out/SealedAuction.sol/SealedAuction.json  /home/timm/go/pkg/mod/github.com/flashbots/suapp-examples@v0.0.0-20241031122241-896ca6742979/out/SealedAuction.sol/SealedAuction.json
+	// go run main.go
+
 	//createKs()
 	//importKs()
 	fmt.Println("1. Deploy Sealed Auction contract")
 	/* 	var _ = deployContractWithConstructor()*/
-	var contract = deployContract()
+	var contract = deployContract(path)
 	fmt.Println("2. Print Info")
 	printInfo(contract)
 	fmt.Println("3. Register RPC endpoint")
@@ -156,5 +219,9 @@ func main() {
 	fmt.Println("4. Get Bidding address for sender")
 	getBiddingAddress(contract)
 	//TODO: place Bid
+	printChainInfoComplete()
 
+	testAccPrivKey := createAccount()
+	fmt.Println("TODO: do something with priv key:", *testAccPrivKey.Priv)
+	makeTransaction(testAccPrivKey)
 }
